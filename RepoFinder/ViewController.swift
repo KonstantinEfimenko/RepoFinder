4566530cea490eb2b5590af4828eff237e2fd62a
Konstantin Efimenko <konstantin.efimenko.0@gmail.com>
2017-09-12 11:46:26 +0300

    Initial Commit

---
+++ RepoFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RepoFinder
+//
+//  Created by Konstantin Efimenko on 9/12/17.
+//  Copyright © 2017 Konstantin Efimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
